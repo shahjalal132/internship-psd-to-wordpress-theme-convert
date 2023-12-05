85cdacceb94a89925e495dfebb17f97c2864277a
Shah jalal <rjshahjalal132@gmail.com>
2023-12-05 16:50:08 +0600

    moved navbar template

--- header.php
+++ header.php
@@ -25,56 +25,6 @@
 
     <!-- ========== Header Section Start ========== -->
     <header class="header-section shadow-sm">
-        <nav class="navbar navbar-expand-md navbar-light bg-white">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="images/logo.png" class="logo-image" alt="logo">
-                </a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto ms-auto mt-2 mt-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Home</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Home 1</a>
-                                <a class="dropdown-item" href="#">Home 2</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Feature</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Testimonial</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Blog</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Blog 1</a>
-                                <a class="dropdown-item" href="#">Blog 2</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex my-2 my-lg-0">
-                        <button class="btn contact-now-btn my-2 my-sm-0" type="submit">Contact Now</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-
+        <?php get_template_part( 'template-parts/header/Navbar' )?>
     </header>
     <!-- ========== Header Section End ========== -->
