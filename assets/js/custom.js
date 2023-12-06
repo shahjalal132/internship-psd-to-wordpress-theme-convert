8dd65f357e844bd1d15e6864e89fee3438549d07
Shah jalal <rjshahjalal132@gmail.com>
2023-12-06 12:58:20 +0600

    slider working

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+(function ($) {
+  $(document).ready(function () {
+    $(".slider").slick({
+      arrows: false,
+      dots: true,
+      infinite: true,
+    });
 
-  $(".slider").slick({
-    arrows: false,
-    dots: true,
-    infinite: true,
+    $(".slick-dots li button").empty();
   });
-
-  $(".slick-dots li button").empty();
-  
-});
+})(jQuery);
--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -8,19 +8,21 @@
 <section id="hero-section">
     <div class="container">
         <div class="slider container">
-            <div class="row slider-children">
-                <div class="slider-texts col-lg-8 col-md-8 col-sm-12">
+
+            <div class="slider-items d-flex">
+                <div class="slider-texts">
                     <h2 class="slider-title">Greatest</h2>
                     <h3 class="slider-subtitle">App Landing Page <br> Your Busienss</h3>
                     <p class="slider-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                     <a href="#" class="btn contact-now-btn my-2 my-sm-0 me-5">Get Demo</a>
                     <a href="#" class="btn contact-slider-btn my-2 my-sm-0">Contact Us</a>
                 </div>
-                <div class="slider-images col-lg-4 col-md-4 col-sm-12">
+                <div class="slider-images">
                     <!-- slider image -->
                     <img src="<?php echo get_template_directory_uri() . '/assets/images/slider-image.png'; ?>" alt="">
                 </div>
             </div>
+            
         </div>
     </div>
     </div>
