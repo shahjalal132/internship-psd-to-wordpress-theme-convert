279b0aa443f3bf8e86a041411d919449f94bd963
Shah jalal <rjshahjalal132@gmail.com>
2023-12-07 14:31:56 +0600

    services section ui added

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php get_header()?>
 
 <!-- Hero Section -->
-<?php get_template_part( "template-parts/hero-section" )?>
+<?php get_template_part( 'template-parts/hero-section' )?>
+
+<!-- Services section -->
+<?php get_template_part( 'template-parts/services-section' )?>
 
 <?php get_footer()?>
---
+++ template-parts/services-section.php
@@ -0,0 +1,56 @@
+<?php 
+/**
+ * Services Section
+ */
+?>
+
+<!-- ========== Start Services section ========== -->
+<section id="services" class="section-padding">
+
+    <div class="services-section-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h2 class="services-heading">How it work</h2>
+                    <h3 class="services-sub-heading">We Provide Service</h3>
+                    <p class="services-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor<br> incididunt ut labore et dolore accumsan lacus vel facilisis.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="services-items">
+        <div class="container p-0">
+            <div class="row service-wrapper">
+
+                <div class="service col-lg-4 col-md-4 col-sm-12 text-center">
+                    <img src="images/service-icon.png" alt="service-icon" class="service-icon">
+                    <h3 class="service-title">Clean Design</h3>
+                    <p class="service-summery">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod te incididunt ut accumsan.</p>
+                    <a href="#" class="service-btn">Read More</a>
+                </div>
+
+                <div class="service col-lg-4 col-md-4 col-sm-12 text-center">
+                    <img src="images/service-icon.png" alt="service-icon" class="service-icon">
+                    <h3 class="service-title">Clean Design</h3>
+                    <p class="service-summery">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod te incididunt ut accumsan.</p>
+                    <a href="#" class="service-btn">Read More</a>
+                </div>
+                
+                <div class="service col-lg-4 col-md-4 col-sm-12 text-center">
+                    <img src="images/service-icon.png" alt="service-icon" class="service-icon">
+                    <h3 class="service-title">Clean Design</h3>
+                    <p class="service-summery">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod te incididunt ut accumsan.</p>
+                    <a href="#" class="service-btn">Read More</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- ========== End Services section ========== -->
