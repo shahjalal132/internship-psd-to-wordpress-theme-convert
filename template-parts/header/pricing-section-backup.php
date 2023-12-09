b26877b00f35a732b512512170b1f86676ac819f
Shah jalal <rjshahjalal132@gmail.com>
2023-12-09 15:05:13 +0600

    services section ui update done and pricing section ui chenge done

---
+++ template-parts/header/pricing-section-backup.php
@@ -0,0 +1,70 @@
+<?php 
+/**
+ * Pricing section
+ */
+?>
+
+<!-- ========== Start pricing section ========== -->
+<section id="pricing-section" class="section-padding">
+    <div class="container">
+
+        <div class="pricing-section-top">
+            <div class="col-sm-12 text-center">
+                <h2 class="services-heading">pricing </h2>
+                <h3 class="services-sub-heading">Get in Reasonable Price</h3>
+                <p class="services-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor<br> incididunt ut labore et dolore accumsan lacus vel facilisis.</p>
+                <div class="subscription">
+                    <a href="#" class="monthly">Monthly</a>
+                    <a href="#" class="yearly">Yearly</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="pricing-items">
+            <div class="container p-0">
+                <div class="row service-wrapper">
+
+                   <?php 
+                        // Start the loop.
+                        $priceInfoFromPricePostType = new WP_Query([
+                            'post_type' => 'pricing',
+                            'posts_per_page' => -1
+                        ]);
+
+                        if ( $priceInfoFromPricePostType->have_posts() ) :
+                            while ( $priceInfoFromPricePostType->have_posts() ) : $priceInfoFromPricePostType->the_post();
+
+                            // get meta box value
+                            $price_plan = get_post_meta( $post->ID, '_price_plan', true );
+                            $amount = get_post_meta( $post->ID, '_amount', true );
+                            $service1 = get_post_meta( $post->ID, '_service1', true );
+                            $service2 = get_post_meta( $post->ID, '_service2', true );
+                            $service3 = get_post_meta( $post->ID, '_service3', true );
+                            $service4 = get_post_meta( $post->ID, '_service4', true );
+                            $service5 = get_post_meta( $post->ID, '_service5', true );
+                            $service6 = get_post_meta( $post->ID, '_service6', true );
+                                ?>
+                                    <div class="service col-lg-4 col-md-4 col-sm-12 text-center">
+                                        <h4 class="pricing-title"><?php echo $price_plan; ?></h4>
+                                        <h3 class="price"><sup>$</sup><span class="price-amount"><?php echo $amount; ?></span>/m</h3>
+                                        <p class="price-text"><?php echo $service1; ?></p>
+                                        <p class="price-text"><?php echo $service2; ?></p>
+                                        <p class="price-text"><?php echo $service3; ?></p>
+                                        <p class="price-text"><?php echo $service4; ?></p>
+                                        <p class="price-text"><?php echo $service5; ?></p>
+                                        <p class="price-text"><?php echo $service6; ?></p>
+                                        <a href="#" class="service-btn">Buy Now</a>
+                                    </div>
+                                <?php
+                            endwhile;
+                        endif;
+                   ?>
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- ========== End pricing section ========== -->
--- template-parts/pricing-section.php
+++ template-parts/pricing-section.php
@@ -1,70 +1,89 @@
-<?php 
+<?php
 /**
- * Pricing section
+ * Pricing Section Template
  */
 ?>
 
-<!-- ========== Start pricing section ========== -->
-<section id="pricing-section" class="section-padding">
+<!-- ========== Start Pricing Section ========== -->
+<section id="pricing" class="section-padding">
     <div class="container">
 
-        <div class="pricing-section-top">
-            <div class="col-sm-12 text-center">
-                <h2 class="services-heading">pricing </h2>
-                <h3 class="services-sub-heading">Get in Reasonable Price</h3>
-                <p class="services-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor<br> incididunt ut labore et dolore accumsan lacus vel facilisis.</p>
-                <div class="subscription">
-                    <a href="#" class="monthly">Monthly</a>
-                    <a href="#" class="yearly">Yearly</a>
+        <div class="pricing-top">
+            <div class="row justify-content-center">
+                <div class="col-lg-7 text-center">
+                    <h2 class="common-heading">pricing </h2>
+                    <h3 class="common-sub-heading">Get in Reasonable Price</h3>
+                    <p class="common-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore accumsan lacus vel facilisis. </p>
                 </div>
             </div>
         </div>
 
-        <div class="pricing-items">
-            <div class="container p-0">
-                <div class="row service-wrapper">
-
-                   <?php 
-                        // Start the loop.
-                        $priceInfoFromPricePostType = new WP_Query([
-                            'post_type' => 'pricing',
-                            'posts_per_page' => -1
-                        ]);
-
-                        if ( $priceInfoFromPricePostType->have_posts() ) :
-                            while ( $priceInfoFromPricePostType->have_posts() ) : $priceInfoFromPricePostType->the_post();
-
-                            // get meta box value
-                            $price_plan = get_post_meta( $post->ID, '_price_plan', true );
-                            $amount = get_post_meta( $post->ID, '_amount', true );
-                            $service1 = get_post_meta( $post->ID, '_service1', true );
-                            $service2 = get_post_meta( $post->ID, '_service2', true );
-                            $service3 = get_post_meta( $post->ID, '_service3', true );
-                            $service4 = get_post_meta( $post->ID, '_service4', true );
-                            $service5 = get_post_meta( $post->ID, '_service5', true );
-                            $service6 = get_post_meta( $post->ID, '_service6', true );
-                                ?>
-                                    <div class="service col-lg-4 col-md-4 col-sm-12 text-center">
-                                        <h4 class="pricing-title"><?php echo $price_plan; ?></h4>
-                                        <h3 class="price"><sup>$</sup><span class="price-amount"><?php echo $amount; ?></span>/m</h3>
-                                        <p class="price-text"><?php echo $service1; ?></p>
-                                        <p class="price-text"><?php echo $service2; ?></p>
-                                        <p class="price-text"><?php echo $service3; ?></p>
-                                        <p class="price-text"><?php echo $service4; ?></p>
-                                        <p class="price-text"><?php echo $service5; ?></p>
-                                        <p class="price-text"><?php echo $service6; ?></p>
-                                        <a href="#" class="service-btn">Buy Now</a>
-                                    </div>
-                                <?php
-                            endwhile;
-                        endif;
-                   ?>
-                    
+        <div class="pricing-tabs">
+            <div>
+                <div class="filter-controls pb-60">
+                    <div class="d-flex justify-content-center">
+                        <div class="controls-wrapper">
+                            <button type="button" class="control-btn" data-filter=".monthly">Monthly</button>
+                            <button type="button" class="control-btn" data-filter=".yearly">Yearly</button>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="pricing-packages">
+            <div class="row mix-filter-wrapper">
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-package-card monthly">
+                        <h6>Starter plan</h6>
+                        <h3><sup>$</sup><span class="price">1200</span><span class="package-time">/y</span></h3>
+                        <ul class="package-service">
+                            <li class="common-description">Connect 2 Redius Servers</li>
+                            <li class="common-description"> key batching editing</li>
+                            <li class="common-description"> real time & automatic editor</li>
+                            <li class="common-description"> real time monitoring</li>
+                            <li class="common-description"> unlimited altering</li>
+                            <li class="common-description"> 30 days trial no engagment</li>
+                        </ul>
+                        <a href="#" class="buy-now-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-package-card monthly">
+                        <h6>Starter plan</h6>
+                        <h3><sup>$</sup><span class="price">1200</span><span class="package-time">/y</span></h3>
+                        <ul class="package-service">
+                            <li class="common-description">Connect 2 Redius Servers</li>
+                            <li class="common-description"> key batching editing</li>
+                            <li class="common-description"> real time & automatic editor</li>
+                            <li class="common-description"> real time monitoring</li>
+                            <li class="common-description"> unlimited altering</li>
+                            <li class="common-description"> 30 days trial no engagment</li>
+                        </ul>
+                        <a href="#" class="buy-now-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-package-card monthly">
+                        <h6>Starter plan</h6>
+                        <h3><sup>$</sup><span class="price">1200</span><span class="package-time">/y</span></h3>
+                        <ul class="package-service">
+                            <li class="common-description">Connect 2 Redius Servers</li>
+                            <li class="common-description"> key batching editing</li>
+                            <li class="common-description"> real time & automatic editor</li>
+                            <li class="common-description"> real time monitoring</li>
+                            <li class="common-description"> unlimited altering</li>
+                            <li class="common-description"> 30 days trial no engagment</li>
+                        </ul>
+                        <a href="#" class="buy-now-btn">Buy Now</a>
+                    </div>
+                </div>
+
             </div>
         </div>
 
     </div>
 </section>
-<!-- ========== End pricing section ========== -->
+<!-- ========== End Pricing Section ========== -->
