74ed7aa69e9438e739f533ff1533e8c8911b9806
Shah jalal <rjshahjalal132@gmail.com>
2023-12-09 15:42:48 +0600

    services section develop done

--- template-parts/services-section-demo.php
+++ template-parts/services-section-demo.php
@@ -21,7 +21,34 @@
             <div class="services-bottom">
                 <div class="row">
 
-                    <div class="col-lg-4 col-md-6 col-sm-12">
+                    <?php 
+                        // Start the Loop
+                        $services_item = new WP_Query([
+                            'post_type' => 'service',
+                            'posts_per_page' => -1
+                        ]);
+
+                        if ( $services_item->have_posts() ) :
+                            while ( $services_item->have_posts() ) : $services_item->the_post();
+                                ?>
+                                    <div class="col-lg-4 col-md-6 col-sm-12">
+                                        <div class="service-item">
+                                            <div class="service-icon">
+                                                <?php the_post_thumbnail(); ?>
+                                            </div>
+                                            <div class="service-content">
+                                                <h4 class="service-item-heading"><?php the_title(); ?></h4>
+                                                <p class="service-item-description common-description"><?php the_content(); ?></p>
+                                                <a href="#" class="service-read-more">read more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                            endwhile;
+                        endif;
+                    ?>
+
+                    <!-- <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="service-item">
                             <div class="service-icon">
                                 <img class="si-icon" src="images/icon1.png" alt="">
@@ -47,21 +74,7 @@
                                 <a href="#" class="service-read-more">read more</a>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 col-sm-12">
-                        <div class="service-item">
-                            <div class="service-icon">
-                                <img class="si-icon" src="images/icon1.png" alt="">
-                            </div>
-                            <div class="service-content">
-                                <h4 class="service-item-heading">clean design</h4>
-                                <p class="service-item-description common-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te incididunt ut
-                                    accumsan. </p>
-                                <a href="#" class="service-read-more">read more</a>
-                            </div>
-                        </div>
-                    </div>
+                    </div> -->
 
                 </div>
             </div>
